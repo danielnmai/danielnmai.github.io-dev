3b828c2cf77d879d922c36d0c99f7622e1060898
Daniel Mai <danielmai1728@gmail.com>
2018-01-04 22:12:11 -0800

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            This is Daniel Mai Website Under Construction!!!
+         </div>
+      );
+   }
+}
+export default App;
