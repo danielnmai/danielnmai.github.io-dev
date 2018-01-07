057f8f18ffa7c67d107822b22012946299a90696
Daniel Mai <danielmai1728@gmail.com>
2018-01-06 19:59:46 -0800

    confirm reacstrap work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './header/Header';
 import NavBar from './header/components/NavBar';
+import { Button } from 'reactstrap';
 
 class App extends React.Component {
   render() {
     return (
     <div>
       <Header/>
+      <Button color="danger">Danger!</Button>
     </div>);
   }
 }
