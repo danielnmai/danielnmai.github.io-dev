6fe34c92b3acb1b681309caa0ae6d401c1c156ff
Daniel Mai <danielmai1728@gmail.com>
2018-01-08 23:18:33 -0800

    fix full screen background

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import Header from './header/Header';
 import NavBar from './header/components/NavBar';
-import { Button } from 'reactstrap';
-import '../styles/index.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <div className="tiledBackground"></div>      
-          <div>
-            <Header/>
-          </div>
-
-      </div>
-
-    )
+      <div className="bg">
+      <NavBar/>
+    </div>)
   }
 }
 export default App;
