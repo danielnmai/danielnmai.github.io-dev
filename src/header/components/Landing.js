d4ea28af997f183d94fd91fba5615f7198a36022
Daniel Mai <danielmai1728@gmail.com>
2018-01-13 15:06:31 -0800

    uninstall bootstrap 4

--- src/header/components/Landing.js
+++ src/header/components/Landing.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import NavBar from './NavBar';
-import TypedString from './TypedString';
-import './styles/landing.css';
+import NavBarDev from './NavBarDev'
+import TypedString from './TypedString'
+import './styles/landing.css'
 
 
 const Landing = (props) => {
   return (
     <header>
-      <NavBar />
+      {/* <NavBar /> */}
+      <NavBarDev />
     <div className="title">
       <h1>Daniel Mai</h1>
       <TypedString />
---
+++ src/header/components/NavBarDev.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/navBar.css';
+
+class NavBarDev extends React.Component {
+  render(){
+    return (
+      <div className='container'>
+        <h2>I'm a NavBar !!!!</h2>
+      </div>
+    )
+  }
+}
+
+export default NavBarDev;
