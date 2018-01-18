99165edc592475514693d204805948d736f23b92
Daniel Mai <danielmai1728@gmail.com>
2018-01-17 22:44:49 -0800

    add profile pic to About page

--- src/center/components/About.js
+++ src/center/components/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/about.css';
+import ProfilePic from '../styles/images/profile_pic.jpg';
 
 class About extends Component {
   render() {
@@ -11,7 +12,7 @@ class About extends Component {
           <p>I am a Software Developer with computer science degree and experience with both hardware and software technologies. I have a true passion for solving human problems. In the past, I achieved this through providing hardware solutions. In the present, I write elegant code to make Internet a better place for people.</p>
         </div>
         <div className='profile_pic'>
-          <p>This is my picture!</p>
+          <img src={ProfilePic}></img>
         </div>
       </div>
     )
