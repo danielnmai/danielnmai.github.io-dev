82c6d3b40c6a5aaeaab97c31915554ec309b399b
Daniel Mai <danielmai1728@gmail.com>
2018-01-22 21:05:52 -0800

    fix about page background color

--- src/center/components/About.js
+++ src/center/components/About.js
@@ -8,7 +8,7 @@ class About extends Component {
       <div className='about'>
         <div className='bio'>
           <h1>About</h1>
-          <h2>Hello!</h2>
+          {/* <h2>Hello!</h2> */}
           <p>I am a Software Developer with computer science degree and experience with both hardware and software technologies. I have a true passion for solving human problems. In the past, I achieved this through providing hardware solutions. In the present, I write elegant code to make Internet a better place for people.</p>
         </div>
         <div className='profile_pic'>
