1eba861d7bcf3de1e7e870b4e88db0d675edac4c
Daniel Mai <danielmai1728@gmail.com>
2018-03-04 20:14:46 -0800

    fix CSS flexbox layout for about page

--- src/center/components/About.js
+++ src/center/components/About.js
@@ -4,12 +4,60 @@ import ProfilePic from '../styles/images/profile_pic.jpg';
 
 class About extends Component {
   render() {
-    return (<div className='about'>
-      <div className='bio'>
-        <h1>About</h1>
-        <p>I am a Software Developer with computer science degree and experience with both hardware and software technologies. I have a true passion for solving human problems. In the past, I achieved this through providing hardware solutions. In the present, I write elegant code to make Internet a better place for people.</p>
+    return (
+      <div className='about'>
+        <div className='bio'>
+          <h1>About</h1>
+            <p>I am a Software Developer with computer science degree and experience with both hardware and software technologies. I have a true passion for solving human problems. In the past, I achieved this through providing hardware solutions. In the present, I write elegant code to make Internet a better place for people.</p>
+        </div>
+        <div className='profile_pic'>
+          <img src={ProfilePic}></img>
+        </div>
+        <div className='skills'>
+          <h1>Skills</h1>
+          <div className='skill-item'>
+            <h3>Languages</h3>
+            <ul>
+              <li>Javascript</li>
+              <li>Ruby</li>
+              <li>Java</li>
+              <li>HTML5/CSS3</li>
+            </ul>
+          </div>
 
-        <h1>Skills</h1>
+          <div className='skill-item'>
+            <h3>Frameworks/ Libraries</h3>
+            <ul>
+              <li>React-Redux</li>
+              <li>Rails</li>
+              <li>Material-UI</li>
+              <li>Node.js</li>
+            </ul>
+          </div>
+
+          <div className='skill-item'>
+            <h3>Database</h3>
+            <ul>
+              <li>PostgreSQL</li>
+              <li>MySQL</li>
+              <li>ActiveRecord</li>
+            </ul>
+          </div>
+
+          <div className='skill-item'>
+            <h3>Other</h3>
+            <ul>
+              <li>Git/Github</li>
+              <li>Heroku</li>
+              <li>webpack</li>
+              <li>AJAX</li>
+              <li>RESTful APIs</li>
+              <li>JSX</li>
+            </ul>
+          </div>
+        </div>
+
+      {/* <h1>Skills</h1>
         <div className='skills'>
           <div className='skill-item'>
             <h3>Languages</h3>
@@ -49,11 +97,9 @@ class About extends Component {
               <li>JSX</li>
             </ul>
           </div>
-        </div>
-      </div>
-      <div className='profile_pic'>
-        <img src={ProfilePic}></img>
-      </div>
+        </div> */
+      }
+
     </div>)
   }
 }
