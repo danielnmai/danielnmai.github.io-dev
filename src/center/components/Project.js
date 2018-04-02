0bef74ef92339a1c4b6d3231797f86025755a66c
Daniel Mai <danielmai1728@gmail.com>
2018-04-02 09:21:00 -0700

    add heroku and github links for projects

--- src/center/components/Project.js
+++ src/center/components/Project.js
@@ -12,14 +12,14 @@ class Project extends Component {
         <div className='grid-container'>
           <div className='item 1'>
             <img src={mBudget}></img>
-            <a href='/home'>Github</a>
-            <a href='/home'>Heroku</a>
+            <a href='https://github.com/danielnmai/budget-webclient' target='_blank'>Github</a>
+            <a href='http://mbudget.herokuapp.com/' target='_blank'> Heroku</a>
           </div>
 
           <div className='item 2'>
             <img src={eCommerce}></img>
-            <div>Github</div>
-            <div>Heroku</div>
+            <a href='https://e-commerce-mini-app.herokuapp.com/' target='_blank'>Heroku</a>
+            <a href='https://github.com/danielnmai/mini-capstone' target='_blank'>Github</a>
           </div>
         </div>
       </div>
