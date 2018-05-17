e96062db8176a906ec89da7082775b264ca7d961
Daniel Mai <danielmai1728@gmail.com>
2018-05-17 15:54:32 -0700

    finish up project layout and put grid to contact form

--- src/center/components/Contact.js
+++ src/center/components/Contact.js
@@ -4,7 +4,7 @@ import '../styles/contact.css';
 class Contact extends Component {
   constructor(props){
     super(props)
-    this.state = {name: 'Bruce Lee', email: '', message: ''}
+    this.state = {name: '', email: '', message: ''}
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
@@ -25,24 +25,27 @@ class Contact extends Component {
  }
   render(){
     return (
-      <div className='contact'>
-          <h1>Contact</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label>Name</label>
-            <input type='text' name='name' value={this.state.name} onChange={(e) => this.handleNameChange(e)} />
-          </div>
-          <div>
-            <label>Email</label>
-            <input type='text' name='email' value={this.state.email} onChange={(e) => this.handleEmailChange(e)}/>
-          </div>
-          <div>
-            <label>Message</label>
-            <textarea type='text' name='message' value={this.state.message} onChange={(e) => this.handleMessageChange(e)} />
-          </div>
-          <input type='submit' value='Send Message' />
-        </form>
+      <div className='container'>
+        <div className='form'>
+            <h1>Contact</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <input type='text' name='name' placeholder="Name" value={this.state.name} onChange={(e) => this.handleNameChange(e)} />
+            </div>
+            <div>
+              <input type='text' name='email' placeholder="Email" value={this.state.email} onChange={(e) => this.handleEmailChange(e)}/>
+            </div>
+            <div>
+              <textarea type='text' name='message' placeholder="Message" value={this.state.message} onChange={(e) => this.handleMessageChange(e)} />
+            </div>
+            <input type='submit' value='Send Message' />
+          </form>
+        </div>
+        <div className='info'>
+          <div>Some Info here</div>
+        </div>
       </div>
+
     )
   }
 }
