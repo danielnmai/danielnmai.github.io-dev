c6eea97572e105a0110190f42ad3a9786a6d22f2
Daniel Mai <danielmai1728@gmail.com>
2018-06-05 14:49:01 -0700

    change form state from React state to mobx

--- src/center/components/Contact.js
+++ src/center/components/Contact.js
@@ -1,57 +1,61 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 import '../styles/contact.css';
 
 
 
 @observer
 class Contact extends Component {
+  @observable formInput = null;
+
   constructor(props) {
     super(props)
-    this.state = { name: '', email: '', message: '' }
+     this.formInput = { formSubmitted: false, name: '', email: '', message: '' }
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(event) {
     event.preventDefault()
     console.log('form is submitted')
-    console.log(this.state)
+    this.formInput.formSubmitted = true
+    console.log(this.formInput)
 
   }
   handleNameChange = (event) => {
-    this.setState({ name: event.target.value })
+    this.formInput.name = event.target.value
   }
   handleEmailChange = (event) => {
-    this.setState({ email: event.target.value })
+    this.formInput.email = event.target.value
   }
   handleMessageChange = (event) => {
-    this.setState({ message: event.target.value })
+    this.formInput.message = event.target.value
   }
 
- 
+  
   render() {
     return (
       <div className='contact'>
         <h1>Contact</h1>       
         <div className='body'>
             <form onSubmit={this.handleSubmit}>            
-                <input className='form-input' type='text' name='name' placeholder="Name" value={this.state.name} onChange={ this.handleNameChange} />
-                <input className='form-input' type='text' name='email' placeholder="Email" value={this.state.email} onChange={this.handleEmailChange} />
-                <textarea className='form-input' type='text' name='message' placeholder="Message" value={this.state.message} onChange={ this.handleMessageChange} />
+                <input className='form-input' type='text' name='name' placeholder="Name" value={this.formInput.name} onChange={ this.handleNameChange} />
+                <input className='form-input' type='text' name='email' placeholder="Email" value={this.formInput.email} onChange={this.handleEmailChange} />
+                <textarea className='form-input' type='text' name='message' placeholder="Message" value={this.formInput.message} onChange={ this.handleMessageChange} />
               <input className='form-input' id='submit-button' type='submit' value='Send Message' />
             </form>
           <div className='info'>
             <h3>Daniel Mai</h3>
             <p> Milpitas, California</p>
             <p> danielmai1728@gmail.com</p>
-            <FormOutput {...this.state}/>
-         
+            {this.formInput.formSubmitted && <FormOutput {...this.formInput} />}        
           </div>
         </div>
       </div>
     )
   }
 }
+
 const FormOutput = (props) => {
   return (
     <div>
